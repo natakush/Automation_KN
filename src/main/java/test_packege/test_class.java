77583e1889d4af5b75d088c0ea6639212d66f526
nata <natakush.job@gmail.com>
2023-11-29 20:06:20 +0200

    hw

---
+++ src/main/java/test_packege/test_class.java
@@ -0,0 +1,9 @@
+package test_packege;
+
+import com.github.javafaker.Faker;
+
+public class test_class {
+    public static void main(String[] args) {
+System.out.println(Faker.instance().artist().name());
+    }
+}
